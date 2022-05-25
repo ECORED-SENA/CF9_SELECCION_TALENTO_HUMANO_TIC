600707fb07badb5545bd7dbbb4a967d89bb6383d
Daniel Mutis <67302227+MagdiKhalifa@users.noreply.github.com>
2022-05-25 16:59:06 -0500

    Ajustes repositorio

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -19,10 +19,7 @@
     
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/CwxgjkH1IoI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)      
-      
-
-     
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/4cMnMM2j2zg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)      
     
 
     
